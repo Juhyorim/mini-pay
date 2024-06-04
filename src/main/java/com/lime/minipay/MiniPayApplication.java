c038940edaea5d941dc939d89456ea4ef801b02d
Juhyorim <joohr1234@gmail.com>
2024-06-04 19:01:47 +0900

    🎉 Initial commit

---
+++ src/main/java/com/lime/minipay/MiniPayApplication.java
@@ -0,0 +1,13 @@
+package com.lime.minipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniPayApplication.class, args);
+    }
+
+}
